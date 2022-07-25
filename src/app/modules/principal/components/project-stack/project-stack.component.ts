013e49b13c00a6a56a42ad4cd7006ff25358d91b
kevinVasquez2 <kevin.vasquez@warespft.com.sv>
2022-07-25 17:49:20 -0600

    🚧 build finis page projects

--- src/app/modules/principal/components/project-stack/project-stack.component.ts
+++ src/app/modules/principal/components/project-stack/project-stack.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectStack } from '../../domain/project-stack';
 
 @Component({
   selector: 'app-project-stack',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectStackComponent implements OnInit {
 
+  projectStackList: ProjectStack[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this._init();
+  }
+
+  _init(): void {
+    this.projectStackList = [
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+      {
+        name: 'Mundo Terrenos cl',
+        image: '/assets/images/resources/projects/mundoterrenos.png',
+        alt: 'image Mundo Terrenos cl',
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ea officiis tenetur sunt
+        obcaecati rerum quidem iusto sapiente perspiciatis amet asperiores minus dolores labore illo
+        sed? Voluptas ipsam molestiae nulla?`,
+        tech: ['HTML', 'JavaScript'],
+        nameSite: 'MundoTerrenos.Cl',
+        site: 'https:waresoft.com.sv'
+      },
+    ];
   }
 
 }
---
+++ src/app/modules/principal/domain/project-stack.ts
@@ -0,0 +1,9 @@
+export interface ProjectStack {
+    name: string,
+    image: string,
+    alt: string,
+    description: string,
+    tech: string[],
+    nameSite: string,
+    site: string
+}
