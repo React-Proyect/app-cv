3c5af0ecff3386c1a754852aaeaa77722948caa5
kevin.aquino <ahorasoysegundo@gmail.com>
2022-08-03 19:49:35 -0600

    ✅ Home Page responsive

--- src/app/modules/shared/template/header/header.component.ts
+++ src/app/modules/shared/template/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { HeaderNavItem } from 'src/app/config/data/menu';
 import { HeaderNav } from '../domain/header-nav';
 
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
     public menuNav: HeaderNav[] = [];
 
     @ViewChild('menuMobilModal') menuMobilModal: ElementRef | null = null;
+    @ViewChild('headerNav') headerNav: ElementRef | null = null;
 
 
     constructor() { }
@@ -36,6 +37,20 @@ export class HeaderComponent implements OnInit {
         const modal = this.menuMobilModal?.nativeElement;
         modal.style.transition = "0.5s";
         modal.style.transform = "translate(-100vw)";
+    }
+
+    @HostListener('window:scroll', []) onWindowScroll(): void {
+        const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        const upButton = this.headerNav?.nativeElement;
+        if (verticalOffset > 100) {
+            upButton.style.position = 'fixed';
+            upButton.style.top = '0';
+            upButton.style.left = '0';
+            upButton.style.animation = 'HeaderGradientBoxShadow 5s infinite';
+        } else {
+            upButton.style.position = 'relative';
+            upButton.style.animation = '';
+        }
     }
 
 }
